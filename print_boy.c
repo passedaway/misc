abd73500bfe1780240549e6d8f8463bed98e9758
passedaway <changqing.1230@gmail.com>
2013-03-20 16:33:16 +0800

    modify some file ( first commit )

--- print_boy.c
+++ print_boy.c
@@ -3,36 +3,14 @@
 #include <string.h>
 #include <stdlib.h>
 
-#if 1
-typedef struct{
-	union{
-		unsigned char mac;
-		long freq;
-	};
-
-	union{
-		unsigned char tmp;
-		int freq;
-	}a;
-	
-//	int freq;/* this will be error, because the first union not have name, so the freq is second define in here */
-}test_t;
-#endif
-
 void show_boy(void)
 {
-	test_t test;
 	char *ptr = NULL;
 	char *buf=malloc(2048);
 	ptr = buf;
 	if( ptr == NULL )
 		return;
 	memset(buf,0, 2048);
-
-#if 1
-	test.mac = 1;
-	printf("test mac=%d test.freq = %d\n", test.mac, test.freq);
-#endif
 	
 	ptr +=sprintf(buf,"                                 \\\\\\|///\n");
 	ptr +=sprintf(ptr,"                               \\\\  - -  //\n");
@@ -54,9 +32,8 @@ void show_boy(void)
 	buf = NULL;
 }
 
-int main(int argc, char **argv)
+int main(void)
 {
-	printf("argc = %d\n",argc);
 	show_boy();
 	return 0;
 }
