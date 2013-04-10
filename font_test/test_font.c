b93e9a5f57c17e9bf2e4488fe020938b2cab6b8e
tain <changqing.1230@gmail.com>
2013-04-10 15:42:10 +0800

    add font:ascii & HZK16

--- font_test/test_font.c
+++ font_test/test_font.c
@@ -20,6 +20,9 @@
 #include <fcntl.h>
 #include <stdlib.h>
 
+#define CHINA_DAT "HZK16.dat"
+#define ASCII_DAT "ASC168.dat"
+
 static void usage(void)
 {
 	printf("\nbecause the vim is use unicode, not gb2312, \n"
@@ -28,42 +31,76 @@ static void usage(void)
 			"then use here\n\n");
 }
 
+static void show_china(unsigned char *data);
+static void show_ascii(unsigned char c);
+static int show_char(unsigned char *data);
+
 int main(int argc, char **argv)
 {
-	unsigned char buf[32];
-	int fd, i, j, k, offset;
-	char *filename = argv[1]?argv[1]:"HZK16.dat";
-
-#if 1
-	//unsigned char str[] = {0xCE, 0xD2};/* wo */
-	unsigned char str[] = {0xd0, 0xb4};/* xie */
-	//unsigned char str[] = {0xA3, 0xA1};/* ! */
-
-	//房 0xB7BF gb2311
-#else
-	unsigned char *str = argv[2] ? argv[2] : "我";
-#endif
+	unsigned char data[] = {
+		0xce, 0xd2, /* wo */
+		0xd4, 0xda, /* zai */
+		0xd0, 0xb4, /* xie */
+		0xb4, 0xfa, /* dai */
+		0xc2, 0xeb, /* ma */
+		0xa3, 0xa1 /* ! */
+		,'I', 0x61, 0x6d,
+		'C', 'o', 'D', 'I', 'N', 'G', '!'
+		,0
+	};
+	unsigned char *str = data;
+	int ret = 0;
 
 	usage();
 	
-	fd = open(filename, O_RDWR);
+	printf("str = %s\n", data);
+	while(1){
+		ret = show_char(str);
+		if ( ret == 0 )
+			break;
+
+		str += ret;
+		printf("\n");
+	}
+
+	return 0;
+}
+
+static int show_char(unsigned char *data)
+{
+	if(data && *data)
+	{
+		if(*data < 0x80)
+		{
+			show_ascii(*data);
+			return 1;
+		}else if( *data > 0xA0 )
+		{
+			show_china(data);
+			return 2;
+		}
+
+		return 0;
+	}
+
+	return 0;
+}
+
+static void show_china(unsigned char *data)
+{
+	unsigned char buf[32];
+	int fd, i, j, k, offset;
+	fd = open(CHINA_DAT, O_RDWR);
 	if( fd < 0 )
 	{
 		perror("cannot open file");
 		exit(-1);
 	}
-
-	printf("str=%s sizeof(str） = %d 0x%02x 0x%02x\n", str, sizeof(str), *str, *(str+1));
-#if 1
-	offset = (94 * (str[0] - 0xa0 - 1) + (str[1] - 0xa0 - 1))*32;
-#else
-	offset = (94 * (0xB7 - 0xa0 - 1) + (0xBF - 0xa0 - 1))*32;
-#endif
+	offset = (94 * (data[0] - 0xa0 - 1) + (data[1] - 0xa0 - 1))*32;
 
 	lseek(fd, offset, SEEK_SET);
 	read(fd, buf, 32);
 	close(fd);
-
 	/*  16 * 16 */
 	for(i=0; i < 16; i++)
 	{
@@ -73,15 +110,43 @@ int main(int argc, char **argv)
 			for(k = 0; k < 8; k++)
 			{
 				if( buf[i*2+j] & (0x80>>k) )
-					printf("%s", "$");
+					printf("$");
 				else
-					printf("%s", " ");
+					printf(" ");
 
 			}
 		}
-		printf("\n");
+		printf("\t0x%02x 0x%02x\n", buf[i*2], buf[i*2+1]);
 	}
-
-	return 0;
 }
 
+static void show_ascii(unsigned char c)
+{
+	unsigned char buf[16];
+	int fd, i, j, offset;
+
+	fd = open(ASCII_DAT, O_RDWR);
+	if( fd < 0 )
+	{
+		perror("cannot open file");
+		exit(-1);
+	}
+	offset = c * 16;
+
+	lseek(fd, offset, SEEK_SET);
+	read(fd, buf, 16);
+	close(fd);
+
+	/*  8 * 16 */
+	for(i=0; i < 16; i++)
+	{
+		for(j = 0; j < 8; j++)
+		{
+			if( buf[i] & (0x80>>j) )
+				printf("#");
+			else
+				printf(" ");
+		}
+		printf("\t\t0x%02x\n", buf[i]);
+	}
+}
